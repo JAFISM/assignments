f357d9cc077b247670fc61f74b60272baa2ab99e
Jafis <jafism2000@gmail.com>
2023-01-03 18:00:04 +0530

    commit

--- lib/Navigation_drawerr_ui.dart
+++ lib/Navigation_drawerr_ui.dart
@@ -1,59 +1,159 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    home:Navdrawer(),
+    home: Navdrawer(),
     debugShowCheckedModeBanner: false,
   ));
 }
-class Navdrawer extends StatelessWidget{
+
+class Navdrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Drawer"),
+        backgroundColor: Colors.deepOrangeAccent,
       ),
-      drawer: Drawer(
+     drawer: Drawer(
         child: Container(
-          decoration:BoxDecoration(gradient: LinearGradient(begin: Alignment.topRight,
-          end: Alignment.bottomCenter,colors: [
-            Colors.deepOrangeAccent,Colors.deepOrange,Colors.orange
-              ])
-          ),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Colors.deepOrangeAccent,
+                Colors.deepOrange,
+                Colors.orange
+              ])),
           child: ListView(
             children: [
-              ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/David bekham.jpg"),
-                  radius: 20,
+              Padding(
+                padding: const EdgeInsets.only(left: 0.1,right: 0.1,top: 50),
+                child: SizedBox(
+                  height: 70,
+                  width: 90,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/DB.jpg"),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      "David Bekham",
+                      style: TextStyle(color: Colors.grey.shade800, fontSize: 20),
+                    ),
+                    subtitle: Text(
+                      "administrator",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                    trailing: Icon(Icons.close),
+                  ),
                 ),
-                title: Text("Terra Crowall",style: TextStyle(color: Colors.white),),
-                trailing: Icon(Icons.close),
+              ),
+              SizedBox(
+                height: 20,
               ),
               const ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home",style: TextStyle(color: Colors.white),),
-                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.home,color: Colors.white,),
+                title: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
                 // onTap:() {Navigator.push(context,MaterialPageRoute(builder:(context)=>Home()));
                 // },
               ),
               const ListTile(
-                leading: Icon(Icons.dashboard),
-                title: Text("DashBord",style: TextStyle(color: Colors.white),),
-                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.dashboard,color: Colors.white,),
+                title: Text(
+                  "DashBord",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
               ),
               const ListTile(
-                leading: Icon(Icons.group),
-                title: Text("Clients",style: TextStyle(color: Colors.white),),
-                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.group,color: Colors.white,),
+                title: Text(
+                  "Clients",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
               ),
-              TextButton(onPressed: (){}, child: child)
+              const ListTile(
+                leading: Icon(Icons.notification_important,color: Colors.white,),
+                title: Text(
+                  "Project",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              const ListTile(
+                leading: Icon(Icons.group_add_rounded,color: Colors.white,),
+                title: Text(
+                  "Partners",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              const ListTile(
+                leading: Icon(Icons.subtitles,color: Colors.white,),
+                title: Text(
+                  "Subbscriptions",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              const ListTile(
+                leading: Icon(Icons.payment,color: Colors.white,),
+                title: Text(
+                  "Payments",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              const ListTile(
+                leading: Icon(Icons.person,color: Colors.white,),
+                title: Text(
+                  "User",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              const ListTile(
+                leading: Icon(Icons.library_books,color: Colors.white,),
+                title: Text(
+                  "Library",
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.white,),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: SizedBox(
+                  height: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10,right: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Logout",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange.shade900,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
 }
