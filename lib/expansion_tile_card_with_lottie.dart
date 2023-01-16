4e7072ae78fb475ca834da22090c9763d1eb45cf
Jafis <jafism2000@gmail.com>
2023-01-16 20:10:45 +0530

    commit

--- lib/expansion_tile_card_with_lottie.dart
+++ lib/expansion_tile_card_with_lottie.dart
@@ -2,51 +2,142 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
+    home: Expansion_tilee(),
     debugShowCheckedModeBanner: false,
-    home: Ex_tile_lottie(),
   ));
 }
-class Ex_tile_lottie extends StatefulWidget{
-  @override
-  State<Ex_tile_lottie> createState() => _Ex_tile_lottieState();
-}
 
-class _Ex_tile_lottieState extends State<Ex_tile_lottie> {
+class Expansion_tilee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.cyan,
-     body:ListView(
-       children: [
-         Center(child: Text("Experience",style: TextStyle(color: Colors.black,fontSize: 50,),)),
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Stack(
-             children: [Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Container(
-                 height: MediaQuery.of(context).size.height/6,
-                 width: MediaQuery.of(context).size.width*6,
-                 decoration: BoxDecoration(
-                     borderRadius: BorderRadius.circular(20),
-                   border: Border.all(width: 5,color: Colors.black)
-
-                 ),
-                 child: Lottie.network(
-                   'https://assets5.lottiefiles.com/packages/lf20_uhnbbycj.json',
-                   width: double.infinity,
-                   height: double.infinity,
-                   fit: BoxFit.cover,
-                 )
-               ),
-             ),
-             ]
-           ),
-         )
-       ],
-     ) ,
-   );
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+                child: Text(
+                  "EXPERIENCE",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.black, fontSize:45),
+                )),
+            Stack(
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),),
+                  child: Lottie.network(
+                      "https://assets6.lottiefiles.com/private_files/lf30_72kogvb9.json",
+                      fit: BoxFit.contain,
+                      height: 200,
+                      width: double.infinity),
+                ),
+                const Positioned(
+                  left: 10,
+                  top: 25,
+                  bottom: 10,
+                  right: 10,
+                  child: ExpansionTile(
+                    title: Text("Flutter Internship",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            fontSize:15)),
+                    subtitle: Text("6 months+",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 10)),
+                    children: [
+                      ListTile(
+                        title: Center(child: Text("Duration : 2 months",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.black),)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Lottie.network(
+                      "https://assets2.lottiefiles.com/packages/lf20_enkk4mul.json",
+                      fit: BoxFit.contain,
+                      height: 200,
+                      width: double.infinity),
+                ),
+                Positioned(
+                  left: 10,
+                  top: 10,
+                  bottom: 10,
+                  right: 10,
+                  child: ExpansionTile(
+                    title: Text("Android Training",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                            fontSize: 15)),
+                    subtitle: Text("2 months",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize:10)),
+                    children: [
+                      Container(
+                        child: ListTile(
+                          title: Center(child: Text("Duration : 2 months",style: TextStyle(fontWeight:FontWeight.bold,color: Colors.black),)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    child: Lottie.network(
+                        "https://assets9.lottiefiles.com/packages/lf20_zh6xtlj9.json",
+                        fit: BoxFit.contain,
+                        height: 200,
+                        width: double.infinity)),
+                const Positioned(
+                  left: 10,
+                  top: 10,
+                  bottom: 10,
+                  right: 10,
+                  child: ExpansionTile(
+                    title: Text("Java Training",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                            fontSize:15)),
+                    subtitle: Text("2 months",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize:10)),
+                    children: [
+                      ListTile(
+                        title: Center(child: Text("Duration : 2 months",style: TextStyle(fontWeight:FontWeight.bold,color: Colors.black),)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
