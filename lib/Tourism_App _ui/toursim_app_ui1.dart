f78eae9e1b79779afa9c04bf83e280607838a459
Jafis <jafism2000@gmail.com>
2023-01-17 00:00:41 +0530

    commit

--- lib/Tourism_App _ui/toursim_app_ui1.dart
+++ lib/Tourism_App _ui/toursim_app_ui1.dart
@@ -32,7 +32,7 @@ class _home1State extends State<home1> {
           children: [
             Container(
               color: Colors.white,
-              height: 100,
+              height: MediaQuery.of(context).size.height*.07,
               width: double.infinity,
               child: Padding(
                 padding: const EdgeInsets.only(left: 15),
@@ -48,20 +48,20 @@ class _home1State extends State<home1> {
             Stack(
               children: [
                 SizedBox(
-                  height: 130,
-                  width: 370,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => home2()));
-                      },
-                      child: Card(
+                  height: MediaQuery.of(context).size.height * .18,
+                  width: MediaQuery.of(context).size.width * .95,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => home2()));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
                         child: Image(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1673809758231-d864d9e35307?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
@@ -85,21 +85,20 @@ class _home1State extends State<home1> {
             Stack(
               children: [
                 SizedBox(
-                  height: 130,
-                  width: 370,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => home2()));
-                      },
-
-                      child: Card(
+                  height: MediaQuery.of(context).size.height * .18,
+                  width: MediaQuery.of(context).size.width * .95,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => home2()));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
                         child: Image(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1673794784636-2e69436d3eee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80"),
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
@@ -123,20 +122,20 @@ class _home1State extends State<home1> {
             Stack(
               children: [
                 SizedBox(
-                  height: 130,
-                  width: 370,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => home2()));
-                      },
-                      child: Card(
+                  height: MediaQuery.of(context).size.height * .18,
+                  width: MediaQuery.of(context).size.width * .95,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => home2()));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
                         child: Image(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1673641688165-6f428e12c51b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"),
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
@@ -160,20 +159,20 @@ class _home1State extends State<home1> {
             Stack(
               children: [
                 SizedBox(
-                  height: 130,
-                  width: 370,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => home2()));
-                      },
-                      child: Card(
+                  height: MediaQuery.of(context).size.height * .18,
+                  width: MediaQuery.of(context).size.width * .95,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => home2()));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
                         child: Image(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"),
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
@@ -197,20 +196,20 @@ class _home1State extends State<home1> {
             Stack(
               children: [
                 SizedBox(
-                  height: 130,
-                  width: 370,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => home2()));
-                      },
-                      child: Card(
+                  height: MediaQuery.of(context).size.height * .18,
+                  width: MediaQuery.of(context).size.width * .95,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => home2()));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
                         child: Image(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1473951574080-01fe45ec8643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1808&q=80"),
-                          fit: BoxFit.cover,
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
